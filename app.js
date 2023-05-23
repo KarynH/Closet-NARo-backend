827d9bcd48f1a7c59695ea0385b963c90349a642
Karyn Huston <huston.karyn@gmail.com>
2023-05-23 12:14:03 -0400

    add route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
+app.get("*", (req, res) => {
+  res.status(404).send("invalid page");
+});
 module.exports = app;
