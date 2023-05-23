e542273d0f20d7e67878c06b3bbc6d1525789aa9
Karyn Huston <huston.karyn@gmail.com>
2023-05-23 12:22:48 -0400

    install mongoDB

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from closet NARo");
+});
+
 app.get("*", (req, res) => {
   res.status(404).send("invalid page");
 });
