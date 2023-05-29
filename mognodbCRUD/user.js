aa5529aa1cc0fc0fc6158c7c997822ad00248446
Karyn Huston <huston.karyn@gmail.com>
2023-05-28 22:23:46 -0400

    insert data collection in user.js file -> cluster is available on mongodb

--- mognodbCRUD/user.js
+++ mognodbCRUD/user.js
@@ -1,7 +1,4 @@
 import { MongoClient } from "mongodb";
-import guestDocument from "../models/guestDocument.js";
-import { ObjectId } from "mongodb";
-
 /**
  * Start by importing the MongoClient object from the mongodb npm package. MongoClient is an object used to initiate the connection with the database.
  */
@@ -26,7 +23,6 @@ export async function connectToCluster(uri) {
   }
 }
 
-//db() , select database
 export async function executeGuestCrudOperations() {
   const uri = process.env.DB_URI;
   let mongoClient;
@@ -38,14 +34,56 @@ export async function executeGuestCrudOperations() {
     const collection = db.collection("guests");
 
     console.log("CREATE GUEST");
-    await createGuestDocument(collection);
+    // await createGuestDocument(collection);
+    const result = await collection.insertOne({
+        email: "karynkhuston@gmail.com",
+        delivery_method: ["USPS first class", "4.20"],
+        first_name: "dev",
+        last_name: "devhuston",
+        address_1: "1 WEST 58th Street",
+        address_2: "",
+        country: "United States",
+        zip_code: "10019",
+        city: "New York",
+        state: "NY",
+        phone_number: "3479128007",
+        card_number: "1234-4563-5653-6543",
+        gift_or_discount_code: "eB66RtY",
+        use_shipping_address: "true",
+        bill_first: "",
+        bill_last: "",
+        bill_address_1: "",
+        bill_address_2: "",
+        bill_country: "",
+        bill_zip_code: "",
+        bill_city: "",
+        bill_state: "",
+        bill_phone_number: "",
+        review_and_purchase: "true",
+        products: [
+          {
+            name: "Product 1",
+            quantity: "2",
+            size: "M",
+            price: "20.00",
+          },
+          {
+            name: "Product 2",
+            quantity: "1",
+            size: "L",
+            price: "15.00",
+          },
+        ],
+        total: "35.00",
+      });
+      console.log(result.insertedId)
   } finally {
     await mongoClient.close();
   }
 }
 
 //create a new document using insertOne()
-export async function createGuestDocument(collection) {
-    guestDocument._id = new ObjectId();
-  await collection.insertOne(guestDocument);
-}
+// export async function createGuestDocument(collection) {
+//     guestDocument._id = new ObjectId();
+//   await collection.insertOne(guestDocument);
+// }
